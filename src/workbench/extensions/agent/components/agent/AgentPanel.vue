e694269eca746226e345c8afa7834baa58b3ee4c
ryan-di <ryan.weihao.di@gmail.com>
2026-08-27 16:44:14 +0800

    feat: clarify agent editable workflow

--- src/workbench/extensions/agent/components/agent/AgentPanel.vue
+++ src/workbench/extensions/agent/components/agent/AgentPanel.vue
@@ -295,7 +295,10 @@ defineExpose({ addAttachment, updateAttachment, removeAttachment })
     <template v-if="!showHistory">
       <footer class="shrink-0 py-3">
         <div class="mx-auto flex w-full max-w-[640px] flex-col gap-4 px-4">
-          <RunNoticeBanner :expanded="isMaximized" />
+          <RunNoticeBanner
+            :expanded="isMaximized"
+            :workflow-name="activeTab?.name"
+          />
           <Composer
             ref="composerRef"
             :streaming="streaming"
